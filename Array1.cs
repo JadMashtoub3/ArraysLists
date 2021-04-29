a3c7c11bc03af09d2855826fcb690f7ede1309b2
Jad Mashtoub <jadmashtoub1@gmail.com>
2021-04-29 13:41:46 +1000

    first

---
+++ Array1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArraysLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            string[] names = new string[5];
+            string[] name = new string[] {"Jad","frank","bob","steve","alex"};
+          
+            System.Console.WriteLine("hello " +name [0]);
+            System.Console.WriteLine("hello " +name [1]);
+            System.Console.WriteLine("hello " +name [2]);
+            System.Console.WriteLine("hello " +name [3]);
+            System.Console.WriteLine("hello " +name [4]);
+        }
+    }
+}
